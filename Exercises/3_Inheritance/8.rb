8638180b23da61e8f0ac5bcc82d280fcb45efa95
Callie Buruchara <callieburuchara@gmail.com>
2020-04-12 10:19:31 +0300

    Finished 3_Inheritance Exercises

---
+++ Exercises/3_Inheritance/8.rb
@@ -0,0 +1,20 @@
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+
+=begin
+METHOD LOOKUP
+Cat
+Animal
+Object 
+Kernel (module inside Object)
+BasicObject
+=end
